01aa0a7a490353a8031e312e20df5f3b5c10777d
Fabio <pizzolatofabio@gmail.com>
2021-08-08 17:26:08 +0200

    add fisrt GET route

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const express = require('express')
 /* COSTRUTTORI */
 const app = express()
 
+/* DATA */
+const mockUserData = [
+    { name: 'Mark' },
+    { name: 'Jill' },
+]
+
+/* ROTTE */
+app.get('/users', function(req, res) {
+    res.json({
+        success: true,
+        message: 'Successfully got users! Nice!',
+        users: mockUserData
+    })
+})
+
 /* SERVER */
 app.listen(8000, function() {
     console.log("Il server funziona!")
