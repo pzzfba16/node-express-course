cad97472a2af64e5dcf74cb4cf4c9dea615693e6
Fabio <pizzolatofabio@gmail.com>
2021-08-09 22:12:56 +0200

    add login POST to the router

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 /* DIPENDENZE */
 const express = require('express')
+const bodyParser = require('body-parser')
 
 /* COSTRUTTORI */
 const app = express()
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 
 /* DATA */
 const mockUserData = [
@@ -10,7 +15,9 @@ const mockUserData = [
     { name: 'Jill' },
 ]
 
-/* ROTTE */
+/* ROTTE 
+GET che trova tutti gli utenti
+*/
 app.get('/users', function(req, res) {
     res.json({
         success: true,
@@ -19,15 +26,38 @@ app.get('/users', function(req, res) {
     })
 })
 
+// GET che trova un utente in base all':id
 app.get('/users/:id', function(req, res) {
-        console.log(req.params.id)
+    console.log(req.params.id)
+    res.json({
+        success: true,
+        message: 'Utente trovato!',
+        user: req.params.id
+    })
+})
+
+// POST che fa il login dell'utente
+app.post('/login', function(req, res) {
+    const userName = req.body.username
+    const password = req.boby.password
+    const mockUsername = 'billyTheKill'
+    const mockPassword = 'superSecret'
+
+    if (userName === mockUsername && password === mockPassword) {
         res.json({
             success: true,
-            message: 'Utente trovato!',
-            user: req.params.id
+            message: 'Password and Username match!',
+            token: 'encrypted token goes here'
         })
-    })
-    /* SERVER */
+    } else {
+        res.json({
+            success: false,
+            message: 'Password and Username do not match!
+        })
+    }
+})
+
+/* SERVER */
 app.listen(8000, function() {
     console.log("Il server funziona!")
 })
